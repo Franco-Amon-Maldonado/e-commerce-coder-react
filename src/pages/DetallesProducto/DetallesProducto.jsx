9ec0dab105dbfbbea22298c38c49e848c7d3b54b
Franco Amon Maldonado <franco.m_887@outlook.com>
2023-10-14 14:05:26 -0300

    14/10/2023
    -useNavigate

--- src/pages/DetallesProducto/DetallesProducto.jsx
+++ src/pages/DetallesProducto/DetallesProducto.jsx
@@ -1,15 +1,18 @@
 import axios from 'axios'
 import { useState } from 'react'
 import { useEffect } from 'react'
-import { useParams } from 'react-router-dom'
+import { Link, useParams, useNavigate } from 'react-router-dom'
 import DetalleProducto from '../../components/DetalleProducto/DetalleProducto'
 import Spinner from '../../components/Spinner/Spinner'
+import IconBack from './IconBack'
 
 function DetallesProducto() {
 	const [producto, setProducto] = useState({})
 	const [spinner, setSpinner] = useState(false)
 
 	let { id } = useParams()
+
+	const navegate = useNavigate()
 
 	async function getProducto() {
 		setSpinner(true)
@@ -23,17 +26,20 @@ function DetallesProducto() {
 		}
 	}
 
-
 	useEffect(() => {
 		getProducto()
 	}, [id])
 
 	return (
 		<main className="container">
-			<h1 className='text-2xl [color:#1976D2] font-extrabold text-center mt-10 mx-auto [width:70%;] bg-white border p-3 rounded-2xl shadow-md uppercase'>Detalles del producto</h1>
-			
-			<div className="grid grid-cols-3 gap-4 p-10">
-				{spinner ? <Spinner /> : producto && producto.id ? <DetalleProducto producto={producto} /> : null}
+			<h1 className="text-base md:text-2xl [color:#1976D2] font-extrabold text-center mt-10 mx-auto [width:70%;] bg-white border p-3 rounded-2xl shadow-md uppercase">
+				Detalles del producto
+			</h1>
+			<Link onClick={() => navegate(-1)} className="flex items-center gap-3 font-bold mt-5">
+				<IconBack /> Volver
+			</Link>
+			<div className='flex justify-center'>
+				{spinner ? <Spinner />  : producto && producto.id ? <div className="grid grid-cols-1 md:grid-cols-3 gap-4 p-10"><DetalleProducto producto={producto} /></div>  : null}
 			</div>
 		</main>
 	)
