7e374579f89b6c974b3ef45012ec66ad0520c3b7
Franco Amon Maldonado <franco.m_887@outlook.com>
2023-10-14 14:05:51 -0300

    14/10/2023
    -Componente svg back

---
+++ src/pages/DetallesProducto/IconBack.jsx
@@ -0,0 +1,21 @@
+function IconBack() {
+	return (
+		<svg
+			className="w-7 h-7 text-[#1976D2] dark:[#1976D2]"
+			aria-hidden="true"
+			xmlns="http://www.w3.org/2000/svg"
+			fill="none"
+			viewBox="0 0 14 10"
+		>
+			<path
+				stroke="currentColor"
+				strokeLinecap="round"
+				strokeLinejoin="round"
+				strokeWidth="2"
+				d="M13 5H1m0 0 4 4M1 5l4-4"
+			/>
+		</svg>
+	)
+}
+
+export default IconBack
