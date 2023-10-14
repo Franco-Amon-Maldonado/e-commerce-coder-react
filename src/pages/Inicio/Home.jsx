5030f8676d1b3be287763b81fc9ab693f081c9f9
Franco Amon Maldonado <franco.m_887@outlook.com>
2023-10-14 13:17:58 -0300

    14/10/2023
    -Estilos varios

--- src/pages/Inicio/Home.jsx
+++ src/pages/Inicio/Home.jsx
@@ -3,8 +3,8 @@ import ItemListContainer from '../../components/ItemListContainer/ItemListContai
 function Home() {
 	return (
 		<main className="mt-10">
-			<h1 className="text-base md:text-3xl bg-white text-center [font-weight:900] mb-10 [width:70%] m-auto border p-3 rounded-2xl shadow-md">
-				Los mejores productos
+			<h1 className="text-base md:text-2xl [color:#1976D2] bg-white text-center [font-weight:900] mb-10 [width:70%] m-auto border p-3 rounded-2xl shadow-md uppercase">
+				Todos nuestros productos
 			</h1>
 			<div className="container m-auto">
 				<ItemListContainer />
