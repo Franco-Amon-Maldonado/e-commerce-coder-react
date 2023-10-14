59f9045a8631ca40979f71fd3b84bb00796c190e
Franco Amon Maldonado <franco.m_887@outlook.com>
2023-10-14 13:17:39 -0300

    14/10/2023
    -Se filtra la consulta a la api segun la categoria

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,35 +1,37 @@
-import axios from "axios"
-import { useEffect, useState } from "react"
-import CardProducts from "../CardProductos/CardProducts"
+import axios from 'axios'
+import { useEffect, useState } from 'react'
+import CardProducts from '../CardProductos/CardProducts'
+import { useParams } from 'react-router-dom'
 
 function ItemListContainer() {
 	const [productos, setProductos] = useState([])
-	
 
-	async function getProductos(){
-		try{
-			
+	const { category } = useParams()
+
+	async function getProductos() {
+		try {
 			const response = await axios.get('https://fakestoreapi.com/products')
-			setProductos(response.data)
-		}catch(err){
+
+			if (category) {
+				const filterCategory = response.data.filter((categoria) => categoria.category === category)
+				setProductos(filterCategory)
+			} else {
+				setProductos(response.data)
+			}
+		} catch (err) {
 			console.error(err)
 		}
 	}
 
-	useEffect(()=>{
+	useEffect(() => {
 		getProductos()
-	},[])
+	}, [category])
 
-	
-	
 	return (
 		<>
-		<div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3 gap-y-3.5">
-			{productos && productos.map(producto  => (
-				<CardProducts key={producto.id} producto={producto}/>
-			))}
-
-		</div>
+			<div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3 gap-y-3.5">
+				{productos && productos.map((producto) => <CardProducts key={producto.id} producto={producto} />)}
+			</div>
 		</>
 	)
 }
