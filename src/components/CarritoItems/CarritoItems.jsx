22f512ab77013e11d162dbcab0ce696e13965776
Franco Amon Maldonado <franco.m_887@outlook.com>
2023-11-05 05:01:09 -0300

    5/11/2023
    -Estilos varios

--- src/components/CarritoItems/CarritoItems.jsx
+++ src/components/CarritoItems/CarritoItems.jsx
@@ -1,14 +1,28 @@
-function CarritoItems({carrito}) {
+import { useContext } from 'react'
+import { CarritoContext } from '../../Context/CarritoContext'
 
-    const { title, price, cantidad} = carrito;
+function CarritoItems({ carrito }) {
+	const { title, price, cantidad, image, id } = carrito
+	const { eliminarDelCarrito } = useContext(CarritoContext)
 
-    return ( 
-        <div className="flex flex-col">
-            <h1>{title}</h1>
-            <h1>{price}</h1>
-            <h1>{cantidad}</h1>
-        </div>
-     );
+	return (
+		<>
+			<article className="flex justify-between p-8 gap-3 [min-height:100px]">
+				<img src={image} alt={`Imagen producto ${title}`} className=" w-20 object-center" />
+				<div className="flex gap-10 p-5 w-11/12 border justify-between items-center">
+					<h1>{title}</h1>
+					<h1>{price}</h1>
+					<h1>Cantidad: {cantidad}</h1>
+					<button
+						onClick={() => eliminarDelCarrito(id)}
+						className="bg-red-500 p-3 block text-white font-semibold rounded-xl hover:bg-red-700 transition-colors duration-300"
+					>
+						Eliminar
+					</button>
+				</div>
+			</article>
+		</>
+	)
 }
 
-export default CarritoItems;
+export default CarritoItems
--- src/pages/Carrito/Carrito.jsx
+++ src/pages/Carrito/Carrito.jsx
@@ -9,21 +9,18 @@ function Carrito() {
 	return (
 		<main className="container h-screen">
 			<section className="grid grid-cols-3 mt-10 gap-7 p-10">
-				<div className="bg-white [minHeight:500px] col-span-3 md:col-span-2 flex justify-center items-center">
+				<div className="bg-white [minHeight:500px] col-span-3 md:col-span-2 rounded-md">
+					<h1 className='text-2xl ml-5 p-5 font-bold'>Productos</h1>
 					{carrito.length > 0 ? (
 						carrito.map((carrito) => <CarritoItems key={carrito.id} carrito={carrito} />)
 					) : (
 						<CarritoVacio />
 					)}
-
-					<div>
-						{carrito.length > 0 ? (<button onClick={limpiarCarrito}>Vaciar Carrito</button> ): null}
-						
-					</div>
 				</div>
 
-				<div className="bg-white h-52 col-span-3 md:col-span-1 sticky top-28">
-					<h2 className='font-bold text-2xl text-center p-4 border-b-gray-500'>Resumen de compra</h2>
+				<div className="bg-white h-52 col-span-3 md:col-span-1 sticky top-28 rounded-md">
+					<h2 className="font-bold text-2xl text-center p-4 border-b-gray-500">Resumen de compra</h2>
+					<div>{carrito.length > 0 ? <button onClick={limpiarCarrito}>Vaciar Carrito</button> : null}</div>
 				</div>
 			</section>
 		</main>
