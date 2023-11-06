3a10f0605c75bbe4d0a17c69b40f66076ffca156
Franco Amon Maldonado <franco.m_887@outlook.com>
2023-11-05 23:23:36 -0300

    5/11/2023
    -Orden de compra firebase

--- src/Context/CarritoContext.jsx
+++ src/Context/CarritoContext.jsx
@@ -48,6 +48,7 @@ function CarritoContextComponente({ children }) {
 	}
 
 
+
 	return (
 		<CarritoContext.Provider
 			value={{
--- src/pages/VerificarCompra/VerificarCompra.jsx
+++ src/pages/VerificarCompra/VerificarCompra.jsx
@@ -1,21 +1,44 @@
-import { TextField } from '@mui/material'
-import {useState} from "react"
+import { TextField, dividerClasses } from '@mui/material'
+import { useContext, useState } from 'react'
+import { CarritoContext } from '../../Context/CarritoContext'
+import { serverTimestamp, collection, addDoc } from 'firebase/firestore'
+import { db } from '../../../fireStorageConfig'
+import { Link } from 'react-router-dom'
 
 function VerificarCompra() {
+	const [usuarioDatos, setUsuarioDatos] = useState({
+		nombre: '',
+		telefono: '',
+		email: '',
+	})
 
-    const [datos, setDatos] = useState({
-        nombre: "",
-        telefono:"",
-        email:""
-    })
+	const [ordenId, setOrdenId] = useState(null)
 
-    const handleChangeDatos = (e) => {
-        setDatos({
-            ...datos,
-            [e.target.name] : e.target.value
-        })
-    }
+	const { carrito, calcularTotalCarrito } = useContext(CarritoContext)
 
+	const total = calcularTotalCarrito()
+
+	const handleChangeDatos = (e) => {
+		setUsuarioDatos({
+			...usuarioDatos,
+			[e.target.name]: e.target.value,
+		})
+	}
+
+	const handleSubmit = async (e) => {
+		e.preventDefault()
+		let orden = {
+			buyer: usuarioDatos,
+			items: carrito,
+			total,
+			date: serverTimestamp(),
+		}
+
+		const colleccionOrdenes = collection(db, 'ordenes')
+
+		const respuesta = await addDoc(colleccionOrdenes, orden)
+		setOrdenId(respuesta.id)
+	}
 
 	return (
 		<main className="container h-screen p-10 md:p-0">
@@ -23,21 +46,57 @@ function VerificarCompra() {
 				Finaliza tu compra
 			</h1>
 
-			<form className="bg-white border p-10 rounded-2xl shadow-md uppercase w-full md:[width:50%;] mt-10 mx-auto [height:50%;]">
-				<header>
-					<h1 className="text-xs text-gray-400" >Ingrese sus datos para finalizar la compra</h1>
-				</header>
+			<>
+				{ordenId ? (
+					<div className="bg-white border p-10 rounded-2xl shadow-md w-full md:[width:50%;] mt-10 mx-auto [height:50%;] flex flex-col items-center justify-evenly">
+						<h2 className="text-center font-bold text-3xl">¡Gracias por su compra!</h2>
+						<p className="text-center mt-10 text-sm">
+							Su numero de ticket es: <span className="[color:#2563EB] font-bold text-lg">{ordenId}</span>{' '}
+						</p>
+                        <Link className=' mx-auto block text-slate-50 bg-gradient-to-r uppercase self-end font-semibold from-cyan-500 to-blue-500 hover:bg-gradient-to-bl transition-opacity hover:opacity-80 duration-300 focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 rounded-lg text-sm px-7 py-3 text-center' to="/">Volver al inicio</Link>
+					</div>
+				) : (
+					<form
+						onSubmit={handleSubmit}
+						className="bg-white border p-10 rounded-2xl shadow-md uppercase w-full md:[width:50%;] mt-10 mx-auto [height:50%;]"
+					>
+						<header>
+							<h2 className="text-xs text-gray-400">Ingrese sus datos para finalizar la compra</h2>
+						</header>
 
-				<div className="flex gap-5 flex-col mt-10">
-					<TextField onChange={handleChangeDatos} name="nombre" type="text" id="outlined-basic" label="Nombre" variant="outlined" />
-					<TextField onChange={handleChangeDatos} name="telefono" type="tel" id="outlined-basic" label="Apellido" variant="outlined" />
-					<TextField onChange={handleChangeDatos} name="email" type="email" id="outlined-basic" label="Email" variant="outlined" />
-				</div>
+						<div className="flex gap-5 flex-col mt-10">
+							<TextField
+								onChange={handleChangeDatos}
+								name="nombre"
+								type="text"
+								id="outlined-basic"
+								label="Nombre"
+								variant="outlined"
+							/>
+							<TextField
+								onChange={handleChangeDatos}
+								name="telefono"
+								type="tel"
+								id="outlined-basic"
+								label="Teléfono"
+								variant="outlined"
+							/>
+							<TextField
+								onChange={handleChangeDatos}
+								name="email"
+								type="email"
+								id="outlined-basic"
+								label="Email"
+								variant="outlined"
+							/>
+						</div>
 
-				<button className="mt-5 mx-auto block text-slate-50 bg-gradient-to-r uppercase self-end font-semibold from-cyan-500 to-blue-500 hover:bg-gradient-to-bl transition-opacity hover:opacity-80 duration-300 focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 rounded-lg text-sm px-7 py-3 text-center">
-					Comprar
-				</button>
-			</form>
+						<button className="mt-5 mx-auto block text-slate-50 bg-gradient-to-r uppercase self-end font-semibold from-cyan-500 to-blue-500 hover:bg-gradient-to-bl transition-opacity hover:opacity-80 duration-300 focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 rounded-lg text-sm px-7 py-3 text-center">
+							Comprar
+						</button>
+					</form>
+				)}
+			</>
 		</main>
 	)
 }
