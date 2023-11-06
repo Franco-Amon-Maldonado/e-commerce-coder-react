338189bc29d48881d04ef444401a66f9dfee00b9
Franco Amon Maldonado <franco.m_887@outlook.com>
2023-11-05 22:35:19 -0300

    5/11/2023
    -Estilos items carrito

--- src/components/CarritoItems/CarritoItems.jsx
+++ src/components/CarritoItems/CarritoItems.jsx
@@ -7,15 +7,18 @@ function CarritoItems({ carrito }) {
 
 	return (
 		<>
-			<article className="flex justify-between p-8 gap-3 [min-height:100px]">
-				<img src={image} alt={`Imagen producto ${title}`} className=" w-20 object-center" />
-				<div className="flex gap-10 p-5 w-11/12 border justify-between items-center">
-					<h1>{title}</h1>
-					<h1>{price}</h1>
-					<h1>Cantidad: {cantidad}</h1>
+			<article className="grid grid-cols-auto md:grid-cols-3 p-8 [min-height:100px]">
+				<div className="col-span 3 md:col-span-1 mx-auto">
+					<img src={image} alt={`Imagen producto ${title}`} className=" w-36 object-cover" />
+				</div>
+
+				<div className="flex flex-col lg:flex-row gap-10 col-auto md:col-span-2 p-5 border justify-between items-center">
+					<h2 className='text-sm [text-wrap:balance] text-center'>{title}</h2>
+					<h3 className='text-xl font-semibold'>${price}</h3>
+					<h4 className='text-xs text-center'>Cantidad: {cantidad}</h4>
 					<button
 						onClick={() => eliminarDelCarrito(id)}
-						className="bg-red-500 p-3 block text-white font-semibold rounded-xl hover:bg-red-700 transition-colors duration-300"
+						className="bg-red-500 text-sm p-2 block text-white font-semibold rounded-xl hover:bg-red-700 transition-colors duration-300"
 					>
 						Eliminar
 					</button>
